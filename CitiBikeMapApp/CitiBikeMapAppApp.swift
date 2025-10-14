34d217da6b5d10e2003229efb288fdd92a2e4b14
Chiraphat Techasiri <ctechasiri@gmail.com>
2025-10-14 16:11:17 -0700

    Initial Commit

---
+++ CitiBikeMapApp/CitiBikeMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiBikeMapAppApp.swift
+//  CitiBikeMapApp
+//
+//  Created by Chiraphat Techasiri on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CitiBikeMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
